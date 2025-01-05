59a88432b1fcb7971bf15b743a388d7b161c3e46
Richard Gao | 高靖博 <richardgao2006@gmail.com>
2025-01-05 23:41:11 +0800

    v0.9 updated, see Wiki

---
+++ src/main/java/com/example/kcg_intellij/TableView_Citations.java
@@ -0,0 +1,12 @@
+package com.example.kcg_intellij;
+
+public class TableView_Citations {
+    private final String columnCitation;
+
+    public TableView_Citations(String columnCitation) {
+        this.columnCitation = columnCitation;
+    }
+    public String getColumnCitation() {
+        return columnCitation;
+    }
+}
